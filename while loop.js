3293a9078bd05062ecef46dc38185f127da77c41
rhymermj <rhymermj@gmail.com>
2020-03-03 01:57:07 +1300

    added solution

--- while loop.js
+++ while loop.js
@@ -19,10 +19,19 @@ while(num <= 333){
     num += 2;
 }
 
+console.log("Print all odd bumbers between 300 and 333");
+var num = 300;
+while(num <= 333){
+    if(num % 2 !== 0){
+        console.log(num);
+    }    
+    num++;
+}
+
 console.log("Print all numbers divisible by 5 AND 3 between 5 and 50");
 var num = 5;
 while(num <= 50){
-    if(num % 5 == 0 && num % 3 == 0){
+    if(num % 5 === 0 && num % 3 === 0){
         console.log(num);       
     } 
     num++;   
